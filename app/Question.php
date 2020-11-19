582c8ef28ae4603f5f9b090aa2698a8ac84189f8
easir Maruf <esir@jpremit.com>
2020-11-19 16:19:52 +0600

    Commit message

---
+++ app/Question.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = [
+      'topic_id',
+      'question',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'answer',
+      'code_snippet',
+      'answer_exp',
+      'question_img',
+      'question_video_link'
+    ];
+
+    public function answers() {
+      return $this->hasOne('App\Answer');
+    }
+
+    public function topic() {
+      return $this->belongsTo('App\Topic');
+    }
+ 
+}
